0642f8c8a5c43706de487b34428d441d148bb81d
RahulBGiddale <86781839+RahulBGiddale@users.noreply.github.com>
2025-05-05 23:16:50 +0530

    reverse string

---
+++ src/com/practice/Reverse.java
@@ -0,0 +1,18 @@
+package com.practice;
+
+public class Reverse {
+	
+	public static void rverse(String str) {
+		
+		String revrse ="";
+		for(int i=0;i<str.length();i++) {
+			revrse=str.charAt(i)+revrse
+					;
+		}
+		System.out.println("reverse string is "+revrse);
+	}
+	public static void main(String[] args) {
+		rverse("rahul");
+	}
+
+}
